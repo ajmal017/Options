7ff68ffc1552f947aaf9e2e0eecbb54a46c29176
djiang333 <dannyjiang3@gmail.com>
2020-06-04 15:05:05 -0400

    data scrapper with concurrency v1

---
+++ data_scrapper.py
@@ -0,0 +1,38 @@
+import concurrent.futures
+import data as d
+from questrade_api import Questrade
+import pandas as pd
+import watchlists as w
+import itertools
+import utils as u
+import time
+
+
+def _helper(x):
+    try:
+        df = d.get_opt_df(q, x)
+        return df
+    except Exception as e:
+        pass
+
+def scrape_data(stock_list):
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        f = executor.map(_helper,stock_list)
+        master_df = pd.DataFrame()
+        for i in f:
+            master_df = master_df.append(i)
+    print(master_df)
+    return master_df
+
+if __name__=="__main__":
+    t = 'qgoB_-clitsBIWKFvpVyMjhl5cCFeq_j0'
+    q = Questrade(grant_type=t, refresh_token=t)
+    start = time.perf_counter()
+    watchlist = list(itertools.chain.from_iterable(w.all_watchlist))
+    df = scrape_data(watchlist)
+    u.snap_vols(df,'Daily Snap')
+    end = time.perf_counter()
+    print(f'Finish in {round(end-start,2)} seconds')
+
+
+
